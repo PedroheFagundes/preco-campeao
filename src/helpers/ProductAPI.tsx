bd3bcb90201ae909089dfe0f9b7dbee48e27600e
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-10-14 07:08:58 -0300

    feat: initial setup

---
+++ src/helpers/ProductAPI.tsx
@@ -0,0 +1,10 @@
+const ProductAPI = {
+
+  login: async (email: string, password: string | number) => {
+
+    return {error: 'Funcionalidade incompleta'};
+  }
+
+};
+
+export default () => ProductAPI;
