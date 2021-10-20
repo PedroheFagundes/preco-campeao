630b96933b2e542261b977461e3972e86349cd05
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-10-20 16:54:49 -0300

    feat: home products display

--- src/pages/Home/styled.tsx
+++ src/pages/Home/styled.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const HomeArea = styled.div`
-  height: 100%;
+  height: calc(100% - var(--navBarHeight));
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
 `;
