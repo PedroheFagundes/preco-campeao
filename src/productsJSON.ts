19a1cbc493ea2012832fc7fee9212a69b1ce1b16
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-11-10 21:04:19 -0300

    feat: share screen

--- src/productsJSON.ts
+++ src/productsJSON.ts
@@ -4,9 +4,9 @@ const products = [
     name: "Leite Macuco",
     info: "1 Litro",
     price: [
-      [3.19, "Serra Azul", "11-15-2021", new Date().setHours(0,0,0,0) <= new Date("11-15-2021").getTime() ? "Válido" : "Expirado"],
-      [4.69, "Atacadão", "11-10-2021", new Date().setHours(0,0,0,0) <= new Date("11-10-2021").getTime() ? "Válido" : "Expirado"],
-      [2.69, "Pepê & Gabriel", "12-10-2021", new Date().setHours(0,0,0,0) <= new Date("12-10-2021").getTime() ? "Válido" : "Expirado"],
+      [3.19, "Serra Azul", "01-15-2021", new Date().setHours(0,0,0,0) <= new Date("01-15-2021").getTime() ? "Válido" : "Expirado"],
+      [4.69, "Atacadão", "01-10-2021", new Date().setHours(0,0,0,0) <= new Date("01-10-2021").getTime() ? "Válido" : "Expirado"],
+      [2.69, "Pepê & Gabriel", "02-10-2021", new Date().setHours(0,0,0,0) <= new Date("02-10-2021").getTime() ? "Válido" : "Expirado"],
     ],
     category: [
       "Bebida", "Laticíneo"
@@ -18,7 +18,7 @@ const products = [
     name: "Coca-cola",
     info: "2 Litros",
     price: [
-      [9.19, "Serra Azul", "11-13-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
+      [9.19, "Serra Azul", "01-13-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
       [7.19, "Atacadão", "03-26-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
       [10.19, "Big Blue", "08-31-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
     ],
@@ -32,9 +32,9 @@ const products = [
     name: "Café 3 Corações",
     info: "Extra Forte 500g",
     price: [
-      [2.19, "Big Blue", "11-13-2021", new Date().setHours(0,0,0,0) <= new Date("11-10-2021").getTime() ? "Válido" : "Expirado"],
-      [1.19, "Atacadão", "12-31-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
-      [5.19, "Pepê & Gabriel", "11-12-2021", new Date().setHours(0,0,0,0) <= new Date("12-10-2021").getTime() ? "Válido" : "Expirado"],
+      [2.19, "Big Blue", "01-13-2021", new Date().setHours(0,0,0,0) <= new Date("01-10-2021").getTime() ? "Válido" : "Expirado"],
+      [1.19, "Atacadão", "02-31-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
+      [5.19, "Pepê & Gabriel", "01-12-2021", new Date().setHours(0,0,0,0) <= new Date("02-10-2021").getTime() ? "Válido" : "Expirado"],
     ],
     category: [
       "Bebida", ""
@@ -46,9 +46,9 @@ const products = [
     name: "Peito de Frango",
     info: "1kg",
     price: [
-      [2.19, "Big Blue", "11-13-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
-      [4.19, "Atacadão", "11-13-2021", new Date().setHours(0,0,0,0) <= new Date("12-10-2021").getTime() ? "Válido" : "Expirado"],
-      [1.19, "Serra Azul", "11-13-2021", new Date().setHours(0,0,0,0) <= new Date("04-10-2021").getTime() ? "Válido" : "Expirado"],
+      [2.19, "Big Blue", "01-13-2021", new Date().setHours(0,0,0,0) <= new Date("09-10-2021").getTime() ? "Válido" : "Expirado"],
+      [4.19, "Atacadão", "01-13-2021", new Date().setHours(0,0,0,0) <= new Date("02-10-2021").getTime() ? "Válido" : "Expirado"],
+      [1.19, "Serra Azul", "01-13-2021", new Date().setHours(0,0,0,0) <= new Date("04-10-2021").getTime() ? "Válido" : "Expirado"],
     ],
     category: [
       "frango", "carne"
