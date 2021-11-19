2b42ca1877d0441f76cd2096eb40a7b9676f6f44
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-11-19 07:40:07 -0300

    more products and code cleaning

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -44,6 +44,201 @@ const Page = () => {
 
   const todayDate = new Date().setHours(0, 0, 0, 0);
 
+  //All the logic behind showing products
+  const productShow = productFilter.map((val, key) => {
+    if (
+      new Date(val.price[0][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[1][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[2][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[3][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[4][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[5][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[6][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[7][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[8][2]).setHours(0, 0, 0, 0) < todayDate &&
+      new Date(val.price[9][2]).setHours(0, 0, 0, 0) < todayDate
+    ) {
+      return null;
+    }
+
+    let cheapPrice = val.price.sort(Comparator);
+    while (new Date(cheapPrice[0][2]).setHours(0, 0, 0, 0) < todayDate) {
+      cheapPrice.shift();
+    }
+
+    let dateFormated = new Date(cheapPrice[0][2])
+      .toLocaleDateString("pt-BR")
+      .substring(0, 5);
+    let priceFormated = cheapPrice[0][0].toString().split(".");
+    let weekDay = new Date(cheapPrice[0][2])
+      .toLocaleDateString("pt-BR", { weekday: "long" })
+      .toUpperCase();
+    switch (weekDay) {
+      case "SEGUNDA-FEIRA":
+        weekDay = "SEGUNDA";
+        break;
+      case "TERÇA-FEIRA":
+        weekDay = "TERÇA";
+        break;
+      case "QUARTA-FEIRA":
+        weekDay = "QUARTA";
+        break;
+      case "QUINTA-FEIRA":
+        weekDay = "QUINTA";
+        break;
+      case "SEXTA-FEIRA":
+        weekDay = "SEXTA";
+        break;
+      default:
+        break;
+    }
+
+    let marketFormated = cheapPrice[0][1].toString();
+    if (marketFormated === "Atacadão") {
+      marketLogo = "atacadao";
+    } else if (marketFormated === "Big Blue") {
+      marketLogo = "bigblue";
+    } else if (marketFormated === "Bramil") {
+      marketLogo = "bramil";
+    } else if (marketFormated === "Casa Friburgo") {
+      marketLogo = "casafriburgo";
+    } else if (marketFormated === "Cavalo Preto") {
+      marketLogo = "cavalopreto";
+    } else if (marketFormated === "Gama") {
+      marketLogo = "gama";
+    } else if (marketFormated === "Pepê & Gabriel") {
+      marketLogo = "pepe&gabriel";
+    } else if (marketFormated === "R&E Mercado") {
+      marketLogo = "r&emercado";
+    } else if (marketFormated === "Serra Azul") {
+      marketLogo = "serraazul";
+    } else if (marketFormated === "Tio Dongo") {
+      marketLogo = "tiodongo";
+    }
+
+    return (
+      <div
+        className="template"
+        id={`${val.id}`}
+        key={key}
+        onClick={(e) => {
+          if (eventID === e.currentTarget.id) {
+            setEventID("0");
+          } else {
+            setEventID(e.currentTarget.id);
+          }
+          setShowMoreInfo(!showMoreInfo);
+        }}
+      >
+        <div className="template-inner">
+          <div className="noSelect productsInfoShort">
+            <img src={`/images/products/${val.image}`} alt="" />
+            <div>
+              <span>{val.name}</span>
+              <span>{val.info}</span>
+            </div>
+            <span className="price">
+              <span>R$</span>
+              <span>{priceFormated[0]}</span>
+              <span>
+                ,
+                {priceFormated[1].length === 1
+                  ? priceFormated[1] + 0
+                  : priceFormated[1]}
+              </span>
+            </span>
+          </div>
+          {parseInt(eventID) === val.id ? null : (
+            <div className="down-arrow">
+              <img src={"/images/navbar/down-arrow.png"} alt="" />
+            </div>
+          )}
+          {parseInt(eventID) === val.id ? (
+            <div className="productsInfoLong">
+              <hr />
+              <div>
+                <div className="market">
+                  <img src={`/images/markets/${marketLogo}-logo.png`} alt="" />
+                  <span className="box">{marketFormated}</span>
+                </div>
+                <div className="expireDate">
+                  <span>promoção até</span>
+                  <span>{weekDay}</span>
+                  <span>{dateFormated}</span>
+                </div>
+              </div>
+              <div className="down-arrow">
+                <img src={"/images/navbar/up-arrow.png"} alt="" />
+              </div>
+            </div>
+          ) : null}
+        </div>
+        {parseInt(eventID) === val.id
+          ? cheapPrice.map((cheapPrice, key) => {
+              let moreInfoDate = new Date(cheapPrice[2])
+                .toLocaleDateString("pt-BR")
+                .substring(0, 5);
+
+              let moreInfoMarketLogo: string = "";
+
+              if (cheapPrice[1] === "Atacadão") {
+                moreInfoMarketLogo = "atacadao";
+              } else if (cheapPrice[1] === "Big Blue") {
+                moreInfoMarketLogo = "bigblue";
+              } else if (cheapPrice[1] === "Bramil") {
+                moreInfoMarketLogo = "bramil";
+              } else if (cheapPrice[1] === "Casa Friburgo") {
+                moreInfoMarketLogo = "casafriburgo";
+              } else if (cheapPrice[1] === "Cavalo Preto") {
+                moreInfoMarketLogo = "cavalopreto";
+              } else if (cheapPrice[1] === "Gama") {
+                moreInfoMarketLogo = "gama";
+              } else if (cheapPrice[1] === "Pepê & Gabriel") {
+                moreInfoMarketLogo = "pepe&gabriel";
+              } else if (cheapPrice[1] === "R&E Mercado") {
+                moreInfoMarketLogo = "r&emercado";
+              } else if (cheapPrice[1] === "Serra Azul") {
+                moreInfoMarketLogo = "serraazul";
+              } else if (cheapPrice[1] === "Tio Dongo") {
+                moreInfoMarketLogo = "tiodongo";
+              }
+
+              let priceFormated = cheapPrice[0].toString().split(".");
+
+              return (
+                <div className="moreInfo" key={key}>
+                  {key === 0 ? null : new Date(cheapPrice[2]).getTime() >=
+                    new Date().setHours(0, 0, 0, 0) ? (
+                    <div>
+                      <img
+                        src={`/images/markets/${moreInfoMarketLogo}-logo.png`}
+                        alt=""
+                      />
+                      <span className="moreInfoSpan1">{cheapPrice[1]}</span>
+                      <div className="moreInfoSpan2">
+                        <span>até dia</span>
+                        <span>{moreInfoDate}</span>
+                      </div>
+                      <span className="moreInfoPrice moreInfoSpan3">
+                        <span>R$</span>
+                        <span>{priceFormated[0]}</span>
+                        <span>
+                          ,
+                          {priceFormated[1].length === 1
+                            ? priceFormated[1] + 0
+                            : priceFormated[1]}
+                        </span>
+                      </span>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })
+          : null}
+      </div>
+    );
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <HomeArea>
@@ -63,207 +258,7 @@ const Page = () => {
           </Link>
         </div>
         <div className="productInfoShortArea">
-          {productFilter.map((val, key) => {
-            if (
-              new Date(val.price[0][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[1][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[2][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[3][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[4][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[5][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[6][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[7][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[8][2]).setHours(0, 0, 0, 0) < todayDate &&
-              new Date(val.price[9][2]).setHours(0, 0, 0, 0) < todayDate
-            ) {
-              return null;
-            }
-
-            let cheapPrice = val.price.sort(Comparator);
-            while (
-              new Date(cheapPrice[0][2]).setHours(0, 0, 0, 0) < todayDate
-            ) {
-              cheapPrice.shift();
-            }
-
-            let dateFormated = new Date(cheapPrice[0][2])
-              .toLocaleDateString("pt-BR")
-              .substring(0, 5);
-            let priceFormated = cheapPrice[0][0].toString().split(".");
-            let weekDay = new Date(cheapPrice[0][2])
-              .toLocaleDateString("pt-BR", { weekday: "long" })
-              .toUpperCase();
-            switch (weekDay) {
-              case "SEGUNDA-FEIRA":
-                weekDay = "SEGUNDA";
-                break;
-              case "TERÇA-FEIRA":
-                weekDay = "TERÇA";
-                break;
-              case "QUARTA-FEIRA":
-                weekDay = "QUARTA";
-                break;
-              case "QUINTA-FEIRA":
-                weekDay = "QUINTA";
-                break;
-              case "SEXTA-FEIRA":
-                weekDay = "SEXTA";
-                break;
-              default:
-                break;
-            }
-
-            let marketFormated = cheapPrice[0][1].toString();
-            if (marketFormated === "Atacadão") {
-              marketLogo = "atacadao";
-            } else if (marketFormated === "Big Blue") {
-              marketLogo = "bigblue";
-            } else if (marketFormated === "Bramil") {
-              marketLogo = "bramil";
-            } else if (marketFormated === "Casa Friburgo") {
-              marketLogo = "casafriburgo";
-            } else if (marketFormated === "Cavalo Preto") {
-              marketLogo = "cavalopreto";
-            } else if (marketFormated === "Gama") {
-              marketLogo = "gama";
-            } else if (marketFormated === "Pepê & Gabriel") {
-              marketLogo = "pepe&gabriel";
-            } else if (marketFormated === "R&E Mercado") {
-              marketLogo = "r&emercado";
-            } else if (marketFormated === "Serra Azul") {
-              marketLogo = "serraazul";
-            } else if (marketFormated === "Tio Dongo") {
-              marketLogo = "tiodongo";
-            }
-
-            return (
-              <div
-                className="template"
-                id={`${val.id}`}
-                key={key}
-                onClick={(e) => {
-                  if (eventID === e.currentTarget.id) {
-                    setEventID("0");
-                  } else {
-                    setEventID(e.currentTarget.id);
-                  }
-                  setShowMoreInfo(!showMoreInfo);
-                }}
-              >
-                <div className="template-inner">
-                  <div className="noSelect productsInfoShort">
-                    <img src={`/images/products/${val.image}`} alt="" />
-                    <div>
-                      <span>{val.name}</span>
-                      <span>{val.info}</span>
-                    </div>
-                    <span className="price">
-                      <span>R$</span>
-                      <span>{priceFormated[0]}</span>
-                      <span>
-                        ,
-                        {priceFormated[1].length === 1
-                          ? priceFormated[1] + 0
-                          : priceFormated[1]}
-                      </span>
-                    </span>
-                  </div>
-                  {parseInt(eventID) === val.id ? null : (
-                    <div className="down-arrow">
-                      <img src={"/images/navbar/down-arrow.png"} alt="" />
-                    </div>
-                  )}
-                  {parseInt(eventID) === val.id ? (
-                    <div className="productsInfoLong">
-                      <hr />
-                      <div>
-                        <div className="market">
-                          <img
-                            src={`/images/markets/${marketLogo}-logo.png`}
-                            alt=""
-                          />
-                          <span className="box">{marketFormated}</span>
-                        </div>
-                        <div className="expireDate">
-                          <span>promoção até</span>
-                          <span>{weekDay}</span>
-                          <span>{dateFormated}</span>
-                        </div>
-                      </div>
-                      <div className="down-arrow">
-                        <img src={"/images/navbar/up-arrow.png"} alt="" />
-                      </div>
-                    </div>
-                  ) : null}
-                </div>
-                {parseInt(eventID) === val.id
-                  ? cheapPrice.map((cheapPrice, key) => {
-                      let moreInfoDate = new Date(cheapPrice[2])
-                        .toLocaleDateString("pt-BR")
-                        .substring(0, 5);
-
-                      let moreInfoMarketLogo: string = "";
-
-                      if (cheapPrice[1] === "Atacadão") {
-                        moreInfoMarketLogo = "atacadao";
-                      } else if (cheapPrice[1] === "Big Blue") {
-                        moreInfoMarketLogo = "bigblue";
-                      } else if (cheapPrice[1] === "Bramil") {
-                        moreInfoMarketLogo = "bramil";
-                      } else if (cheapPrice[1] === "Casa Friburgo") {
-                        moreInfoMarketLogo = "casafriburgo";
-                      } else if (cheapPrice[1] === "Cavalo Preto") {
-                        moreInfoMarketLogo = "cavalopreto";
-                      } else if (cheapPrice[1] === "Gama") {
-                        moreInfoMarketLogo = "gama";
-                      } else if (cheapPrice[1] === "Pepê & Gabriel") {
-                        moreInfoMarketLogo = "pepe&gabriel";
-                      } else if (cheapPrice[1] === "R&E Mercado") {
-                        moreInfoMarketLogo = "r&emercado";
-                      } else if (cheapPrice[1] === "Serra Azul") {
-                        moreInfoMarketLogo = "serraazul";
-                      } else if (cheapPrice[1] === "Tio Dongo") {
-                        moreInfoMarketLogo = "tiodongo";
-                      }
-
-                      let priceFormated = cheapPrice[0].toString().split(".");
-
-                      return (
-                        <div className="moreInfo" key={key}>
-                          {key === 0 ? null : new Date(
-                              cheapPrice[2]
-                            ).getTime() >= new Date().setHours(0, 0, 0, 0) ? (
-                            <div>
-                              <img
-                                src={`/images/markets/${moreInfoMarketLogo}-logo.png`}
-                                alt=""
-                              />
-                              <span className="moreInfoSpan1">
-                                {cheapPrice[1]}
-                              </span>
-                              <div className="moreInfoSpan2">
-                                <span>até dia</span>
-                                <span>{moreInfoDate}</span>
-                              </div>
-                              <span className="moreInfoPrice moreInfoSpan3">
-                                <span>R$</span>
-                                <span>{priceFormated[0]}</span>
-                                <span>
-                                  ,
-                                  {priceFormated[1].length === 1
-                                    ? priceFormated[1] + 0
-                                    : priceFormated[1]}
-                                </span>
-                              </span>
-                            </div>
-                          ) : null}
-                        </div>
-                      );
-                    })
-                  : null}
-              </div>
-            );
-          })}
+          {productShow}
           <div className="endMessage">
             <img src="/images/logo.png" alt="" />
             <span>
